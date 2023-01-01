1a375e7b3468f7f1878af14f02209a033c93dfdd
Nathan Coxson <nathancoxson2739@gmail.com>
2023-01-01 04:49:22 -0500

    Initialized project structure.

---
+++ src/Components/Panel/Panel.js
@@ -0,0 +1,9 @@
+import './Panel.css'
+
+export function Panel(props) {
+    return (
+        <div className='Panel'>
+            
+        </div>
+    );
+}
---
+++ src/Components/Title/Title.js
@@ -0,0 +1,9 @@
+import './Title.css'
+
+export function Title() {
+    return (
+        <div>
+            <h1>To Do List</h1>
+        </div>
+    );
+}
