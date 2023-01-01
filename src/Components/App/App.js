950e05d8b276fb17c5001336abeef966885e82c7
Nathan Coxson <nathancoxson2739@gmail.com>
2023-01-01 05:19:33 -0500

    Added css to allow for multiple panels that automatically resize themselves.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -21,7 +21,11 @@ function App() {
       </header> */}
       
       <Title />
-      <Panel />
+      <div className="Board">
+        <Panel />
+        <Panel />
+      </div>
+      
     </div>
   );
 }
