9fc687d80568963cabb5597432bf9af0c9ee61c4
Nathan Coxson <nathancoxson2739@gmail.com>
2023-01-08 05:34:37 -0500

    Added the new task button that lets you input title and description for your task. The panel component now tracks the list of tasks in it's state which is updated when new tasks are added.

--- src/Components/Panel/Panel.js
+++ src/Components/Panel/Panel.js
@@ -2,18 +2,69 @@ import './Panel.css'
 import { Task } from '../Task/Task.js'
 import { useEffect, useState } from 'react';
 
-export function Panel(props) {
-    const [tasks, setTasks] = useState();
 
+
+export function Panel(props) {
+    const [addingTask, setAddingTask] = useState(false);
+    const [tasks, setTasks] = useState([
+        // {
+        //     title: 'Math HW',
+        //     description: 'Calc problems'
+        // },
+        // {
+        //     title: 'Engl HW',
+        //     description: 'Write essay'
+        // },
+        // {
+        //     title: 'Chem HW',
+        //     description: 'Lab Report'
+        // }
+    ]);
+
+    //Handler for when the new task button is clicked
     const handleAddTask = (event) => {
-        
+        addingTask ? setAddingTask(false) : setAddingTask(true);
+    }
+
+    //Handler for when a new task's data is typed in.
+    const handleTaskSubmission = (event) => {
+        event.preventDefault();
+        setTasks([
+            ...tasks,
+            {
+                title: event.target.title.value,
+                description: event.target.description.value
+            }
+        ]);
+        setAddingTask(false);
     }
 
     return (
         <div className='Panel'>
-            <Task title='Math HW' description="Calc problems"/>
-            <Task title='Math HW' description="Calc problems"/>
-
+            {
+                tasks.map((task, i) => {
+                    return <Task 
+                        title={task.title} 
+                        description={task.description} 
+                        key={i}
+                    />
+                })
+            }            
+            {addingTask &&
+                <form onSubmit={handleTaskSubmission}>
+                    <input 
+                        type='text'
+                        placeholder='Title'
+                        name='title'>
+                    </input>
+                    <input 
+                        type='text'
+                        placeholder='Description'
+                        name='description'>
+                    </input>
+                    <input type='submit'></input>
+                </form>
+            }
             <button onClick={handleAddTask}>+</button>
         </div>
     );
