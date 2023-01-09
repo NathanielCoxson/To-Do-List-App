cc2303a56029e886cb93cc8c109074ab2cc267bb
Nathan Coxson <nathancoxson2739@gmail.com>
2023-01-09 06:01:40 -0500

    Added new Board component that controls the main task board of the app. Also added the ability to create additional panels as well as remove specific ones.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,33 +1,13 @@
 import './App.css';
 import { Title } from '../Title/Title.js'
-import { Panel } from '../Panel/Panel.js'
+import { Board } from '../Board/Board';
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      
-      <Title />
-      <div className="Board">
-        <Panel />
-        <Panel />
-      </div>
-      
-    </div>
-  );
+        <div className="App">
+            <Title />
+            <Board />
+        </div>
+    );
 }
-
 export default App;
