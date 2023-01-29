ce29dbdaa17615622b76e47faae876f822f80059
Nathan Coxson <nathancoxson2739@gmail.com>
2023-01-29 06:08:20 -0500

    First, the button for adding a new panel was moved into a new component called Header that is rendered inside of the Board component. Then I added a burger button that is used to toggle the sidebar. I also set up logic within the Sidebar component function to show and hide the sidebar as well as any items that are shown inside of it. Currently there is no logic to have multiple different boards so there is placeholder data meant to show what it would like like if this was working.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 //import { Title } from '../Title/Title.js'
 import { Board } from '../Board/Board';
-import { Sidebar } from '../Sidebar/Sidebar';
 
 function App() {
-  return (
+    return (
         <div className="App">
-            {/* <Title /> */}
-            <Sidebar />
             <Board />
         </div>
     );
--- src/Components/Title/Title.js
+++
@@ -1,9 +0,0 @@
-import './Title.css'
-
-export function Title() {
-    return (
-        <div>
-            <h1>To Do List</h1>
-        </div>
-    );
-}
